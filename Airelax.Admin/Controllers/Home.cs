be31bf3eda083bd17dc0ff361a99427f63b385da
peace920902 <peace9209021@gmail.com>
2021-09-23 22:24:32 +0800

    first commit

---
+++ Airelax.Admin/Controllers/Home.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Airelax.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Houses()
+        {
+            return View();
+        }
+
+        public IActionResult Income()
+        {
+            return View();
+        }
+    }
+}
---
+++ Airelax.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Airelax.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
