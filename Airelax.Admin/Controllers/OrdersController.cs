d1a4641ccf6e725f87da08510b1756bcd42421ef
Girro39 <yellyou39@gmail.com>
2021-09-27 00:07:42 +0800

    SHOP-122 OrderController叫用OrderService

--- Airelax.Admin/Controllers/OrdersController.cs
+++ Airelax.Admin/Controllers/OrdersController.cs
@@ -6,5 +6,23 @@ namespace Airelax.Admin.Controllers
     [Route("api/[controller]")]
     public class OrdersController : Controller
     {
+        //叫service做事囉
+        public OrderService _orderService;
+        public OrdersController(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        public IActionResult RevenueBeforeNow()
+        {
+            return View();
+        }
+        public IActionResult RevenueDuringWeek()
+        {
+            return View();
+        }
+        public IActionResult SalesDuringMonth()
+        {
+            return View();
+        }
     }
 }
