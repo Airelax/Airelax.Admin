3f0258afb9389549d06755c6152cf5a6b65a2676
Girro39 <yellyou39@gmail.com>
2021-09-27 00:06:27 +0800

    SHOP-122 創OrderService

---
+++ Airelax.Admin/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using Airelax.Domain.RepositoryInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airelax.Admin
+{
+    public class OrderService
+    {
+        public IOrderRepository _orderRepository;
+        public OrderService(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        //1.轉成時間內訂單總金額
+        //2.一周總金額
+        //3.月銷售額
+
+    }
+}
