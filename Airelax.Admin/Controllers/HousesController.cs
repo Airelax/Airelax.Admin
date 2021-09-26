7f1d6a1f6dc59dad38e225539979c6e424cc7456
ChiaChi-Chang <cyhs211010@gmail,com>
2021-09-26 21:12:34 +0800

    123

--- Airelax.Admin/Controllers/HousesController.cs
+++ Airelax.Admin/Controllers/HousesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Airelax.Domain.Houses;
+using Airelax.Domain.Members;
+using Airelax.Domain.RepositoryInterface;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Airelax.Admin.Controllers
 {
@@ -6,5 +10,28 @@ namespace Airelax.Admin.Controllers
     [Route("api/[controller]")]
     public class HousesController : Controller
     {
+        private readonly IMemberRepository _memberRepository;
+        private readonly IHouseRepository _houseRepository;
+        public HousesController(IMemberRepository memberRepository,IHouseRepository houseRepository)
+        {
+            _memberRepository = memberRepository;
+            _houseRepository = houseRepository;
+        }
+
+        [HttpGet]
+        [Route("GetAllMembers")]//https://localhost:44305/api/Houses/GetAllMembers
+        public IQueryable<Member> GetMembers()
+        {
+            return _memberRepository.GetAll();
+        }
+
+
+        [HttpGet]
+        [Route("GetAllHouses")]//https://localhost:44305/api/Houses/GetAllHouses
+        public IQueryable<House> GetHouses()
+        {
+            return _houseRepository.GetAll();
+        }
+
     }
 }
