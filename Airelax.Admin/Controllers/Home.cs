b2fa9cc21741bd46e60a48e9ebf9757a2774f754
Girro39 <yellyou39@gmail.com>
2021-09-27 00:06:59 +0800

    SHOP-122 叫用OrderController

--- Airelax.Admin/Controllers/Home.cs
+++ Airelax.Admin/Controllers/Home.cs
@@ -6,6 +6,11 @@ namespace Airelax.Admin.Controllers
     public class HomeController : Controller
     {
         // GET
+        public OrdersController _ordersController;
+        public HomeController(OrdersController ordersController)
+        {
+            _ordersController = ordersController;
+        }
         public IActionResult Index()
         {
             return View();
@@ -13,6 +18,11 @@ namespace Airelax.Admin.Controllers
 
         public IActionResult Orders()
         {
+            //叫隔壁ordercontroller做事
+            //1.取一周總金額
+            //2.取訂單銷售額
+            //3.取時間內訂單總金額
+            //4.吐給view渲染
             return View();
         }
 
